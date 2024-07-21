7a5d4b57cf5f5f93607cc73875fc8c7ca11eb471
Yulun <yulunliss001@gmail.com>
2024-07-21 13:09:10 -0400

    Added Auth app 2FA

--- Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -177,7 +177,7 @@ namespace PizzaApp.Areas.Identity.Pages.Account.Manage
             return string.Format(
                 CultureInfo.InvariantCulture,
                 AuthenticatorUriFormat,
-                _urlEncoder.Encode("Microsoft.AspNetCore.Identity.UI"),
+                _urlEncoder.Encode("PizzaApp"),
                 _urlEncoder.Encode(email),
                 unformattedKey);
         }
---
+++ Pages/Reports/Index.cshtml.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaApp.Pages.Reports;
+public class IndexModel : PageModel
+{
+  public IndexModel() { }
+  // public async Task OnGetAsync(){}
+}
