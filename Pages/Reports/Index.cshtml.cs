096348a3a20ce28d04a27bd17b8296f0a34779d4
Yulun <yulunliss001@gmail.com>
2024-07-21 13:36:00 -0400

    Block staff access to reports

--- Pages/Reports/Index.cshtml.cs
+++ Pages/Reports/Index.cshtml.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PizzaApp.Pages.Reports;
+
+[Authorize(Roles = "Owner,Manager")]
 public class IndexModel : PageModel
 {
   public IndexModel() { }
